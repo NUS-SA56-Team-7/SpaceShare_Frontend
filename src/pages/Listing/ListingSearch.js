28a62c76c7474fc1ae9871ec6410c0bc070a9cc0
Stephen Phyo <stephenphyo2019.official@gmail.com>
2023-08-20 04:27:30 +0000

    FINAL VERSION

--- src/pages/Listing/ListingSearch.js
+++ src/pages/Listing/ListingSearch.js
@@ -254,7 +254,20 @@ const ListingSearch = () => {
                                                 "block px-4 py-2 text-sm"
                                             )}
                                         >
-                                            Common Room
+                                            Low to High
+                                        </a>
+                                    )}
+                                </Menu.Item>
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <a
+                                            href="#"
+                                            className={classNames(
+                                                active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                                                "block px-4 py-2 text-sm"
+                                            )}
+                                        >
+                                            High to Low
                                         </a>
                                     )}
                                 </Menu.Item>
